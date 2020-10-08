62121765ed9552d6245acf5f49408776b0b7fe76
Igoryan93 <lost.autumn72@gmail.com>
2020-10-08 18:01:06 +0600

    update task_6

--- task_6.php
+++ task_6.php
@@ -112,7 +112,7 @@
                                             'position' => 'Partner &amp; Contributor',
                                             'email'    => '@lodev09',
                                             'nickname' => 'lodev09',
-                                            'status'   => 'banned'
+                                            'status'   => '1'
                                         ],
                                         [
                                             'img'      => 'roberto.png',
@@ -121,7 +121,7 @@
                                             'position' => 'Partner &amp; Contributor',
                                             'email'    => '@sildur',
                                             'nickname' => 'sildur',
-                                            'status'   => 'banned'
+                                            'status'   => '1'
                                         ],
                                         [
                                             'img'      => 'jovanni.png',
@@ -138,46 +138,41 @@
                                             'position' => 'Partner &amp; Contributor',
                                             'email'    => '@atlantez',
                                             'nickname' => 'Walapa',
-                                            'status'   => 'banned'
+                                            'status'   => '1'
                                         ],
                                     ];
-
-                                    foreach ($arr as $item) {
-                                        if(empty($item['status'])) {
-                                            echo '
-                                            <div class="rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
-                                                <img src="img/demo/authors/'. $item["img"] .'" alt="'. $item["name"] . '" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
-                                                <div class="ml-2 mr-3">
-                                                    <h5 class="m-0">
-                                                        ' . $item["name"] . ' ' . $item["work"] . ' 
-                                                        <small class="m-0 fw-300">
-                                                            ' . $item["position"] . '
-                                                        </small>
-                                                    </h5>
-                                                    <a href="https://twitter.com/' . $item["email"] . ' " class="text-info fs-sm" target="_blank">' . $item["email"] . '</a> -
-                                                    <a href="https://wrapbootstrap.com/user/'. $item["nickname"] . '" class="text-info fs-sm" target="_blank" title="Contact ' . $item["name"] . '"><i class="fal fa-envelope"></i></a>
-                                                </div>
-                                            </div>
-                                        ';
-                                        } else {
-                                            echo '
-                                            <div class="banned rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
-                                                <img src="img/demo/authors/'. $item["img"] .'" alt="'. $item["name"] . '" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
-                                                <div class="ml-2 mr-3">
-                                                    <h5 class="m-0">
-                                                        ' . $item["name"] . ' ' . $item["work"] . ' 
-                                                        <small class="m-0 fw-300">
-                                                            ' . $item["position"] . '
-                                                        </small>
-                                                    </h5>
-                                                    <a href="https://twitter.com/' . $item["email"] . ' " class="text-info fs-sm" target="_blank">' . $item["email"] . '</a> -
-                                                    <a href="https://wrapbootstrap.com/user/'. $item["nickname"] . '" class="text-info fs-sm" target="_blank" title="Contact ' . $item["name"] . '"><i class="fal fa-envelope"></i></a>
-                                                </div>
-                                            </div>
-                                        ';
-                                        }
-                                    }
                                 ?>
+                               <?php foreach ($arr as $item):?>
+                                    <?php if(($item['status'] == 1)): ?>
+                                       <div class="banned rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
+                                           <img src="img/demo/authors/<?php echo $item["img"] ?> " alt="<?php echo $item["name"] ?>" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
+                                           <div class="ml-2 mr-3">
+                                               <h5 class="m-0">
+                                                   <?php echo $item["name"] . ' ' . $item["work"]?>
+                                                   <small class="m-0 fw-300">
+                                                       <?php echo $item["position"] ?>
+                                                   </small>
+                                               </h5>
+                                               <a href="https://twitter.com/<?php echo $item["email"] ?>" class="text-info fs-sm" target="_blank"><?php echo $item["email"] ?></a> -
+                                               <a href="https://wrapbootstrap.com/user/<?php echo $item["nickname"] ?>" class="text-info fs-sm" target="_blank" title="Contact <?php echo $item["name"] ?>"><i class="fal fa-envelope"></i></a>
+                                           </div>
+                                       </div>
+                                    <?php else: ?>
+                                       <div class="rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
+                                           <img src="img/demo/authors/<?php echo $item["img"] ?> " alt="<?php echo $item["name"] ?>" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
+                                           <div class="ml-2 mr-3">
+                                               <h5 class="m-0">
+                                                   <?php echo $item["name"] . ' ' . $item["work"]?>
+                                                   <small class="m-0 fw-300">
+                                                       <?php echo $item["position"] ?>
+                                                   </small>
+                                               </h5>
+                                               <a href="https://twitter.com/<?php echo $item["email"] ?>" class="text-info fs-sm" target="_blank"><?php echo $item["email"] ?></a> -
+                                               <a href="https://wrapbootstrap.com/user/<?php echo $item["nickname"] ?>" class="text-info fs-sm" target="_blank" title="Contact <?php echo $item["name"] ?>"><i class="fal fa-envelope"></i></a>
+                                           </div>
+                                       </div>
+                                    <?php endif; ?>
+                               <? endforeach; ?>
                         </div>
                         </div>
                     </div>
