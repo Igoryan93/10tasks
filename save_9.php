12882fd405fa2111345adc7af5afae836fce29e2
Igoryan93 <lost.autumn72@gmail.com>
2020-10-10 12:19:15 +0600

    update save_9

--- save_9.php
+++ save_9.php
@@ -1,11 +1,9 @@
 <?php
 
 $text = $_POST['text'];
-if(!empty($_POST)) {
-    $text = $_POST['text'];
-    $pdo = new PDO("mysql:host=localhost; dbname=table_users", "root", "root");
-    $db = "INSERT INTO table_text (text) VALUES (:text)";
-    $statement = $pdo->prepare($db);
-    $statement->execute(['text' => $text]);
-    header('Location: task_9.php');
-}
+$text = $_POST['text'];
+$pdo = new PDO("mysql:host=localhost; dbname=table_users", "root", "root");
+$db = "INSERT INTO table_text (text) VALUES (:text)";
+$statement = $pdo->prepare($db);
+$statement->execute(['text' => $text]);
+header('Location: task_9.php');
